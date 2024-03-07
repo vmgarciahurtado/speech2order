0c65d8d3b3b4cb0247643c033d1a9d8305ac8956
vamgarcia <victor.garcia@celuweb.com>
2024-03-07 16:20:17 -0500

    add fuzzy search

--- lib/search.dart
+++ lib/search.dart
@@ -1,6 +1,59 @@
+import 'package:fuzzy/fuzzy.dart';
 import 'package:speech2order/model.dart';
 
 List<Speech2OrderProduct> searchProducts(
+    List<Speech2OrderProduct> productos, List<String> palabrasClave) {
+  // Normalize keywords and product titles
+  palabrasClave = palabrasClave
+      .map((palabra) => palabra
+          .trim()
+          .toLowerCase()
+          .replaceAll(RegExp(r'[áàâãäå]'), 'a')
+          .replaceAll(RegExp(r'[éèêë]'), 'e')
+          .replaceAll(RegExp(r'[íìîï]'), 'i')
+          .replaceAll(RegExp(r'[óòôõöø]'), 'o')
+          .replaceAll(RegExp(r'[úùûü]'), 'u'))
+      .toList();
+
+  productos = productos
+      .map((producto) => Speech2OrderProduct(
+          title: producto.title
+              .toLowerCase()
+              .replaceAll(RegExp(r'[áàâãäå]'), 'a')
+              .replaceAll(RegExp(r'[éèêë]'), 'e')
+              .replaceAll(RegExp(r'[íìîï]'), 'i')
+              .replaceAll(RegExp(r'[óòôõöø]'), 'o')
+              .replaceAll(RegExp(r'[úùûü]'), 'u'),
+          barCode: producto.barCode))
+      .toList();
+
+  // Create Fuzzy instance with options
+  final fuse = Fuzzy(
+    productos.map((p) => p.title).toList(),
+    options: FuzzyOptions(
+      findAllMatches: true,
+      tokenize: true,
+      threshold: 0.5, // Adjust threshold as needed
+    ),
+  );
+
+  // Search for each keyword
+  final results = palabrasClave.map((palabra) => fuse.search(palabra)).toList();
+
+  // Flatten results and sort by score (highest first)
+  final allResults = results.expand((list) => list).toList();
+  allResults.sort((a, b) => b.score.compareTo(a.score));
+
+  // Extract top 20 products (considering duplicates)
+  final topProducts = allResults
+      .take(20)
+      .map((result) => productos.firstWhere((p) => p.title == result.item))
+      .toList();
+
+  return topProducts.toSet().toList(); // Remove duplicates
+}
+
+List<Speech2OrderProduct> searchProducts2(
     List<Speech2OrderProduct> productos, List<String> palabrasClave) {
   // Aplicar trim() a cada palabra clave para eliminar espacios al principio y al final
   palabrasClave = palabrasClave
@@ -46,7 +99,7 @@ List<Speech2OrderProduct> searchProducts(
 }
 
 //? This method search with (tilde)
-List<Speech2OrderProduct> searchProducts2(
+List<Speech2OrderProduct> searchProducts3(
     List<Speech2OrderProduct> productos, List<String> palabrasClave) {
   // Aplicar trim() a cada palabra clave para eliminar espacios al principio y al final
   palabrasClave = palabrasClave.map((palabra) => palabra.trim()).toList();
